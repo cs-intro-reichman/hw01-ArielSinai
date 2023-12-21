3d426a7b1264939952840e7d32ab6effd8d37d47
ArielSinai <ariel.sinai@post.runi.ac.il>
2023-12-21 14:25:45 +0200

    fixing the file name

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,22 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
-public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+import java.lang.Math;
+public class GenThree
+{
+    public static void main(String[] args) 
+	{
+		//define range
+	    int min = Integer.parseInt(args[0]);
+		int max = Integer.parseInt(args[1]);
+		int range = max - min + 1;
+		//create and print 3 random number within the range
+		int rand1 = (int) (Math.random() * range) + min;
+		System.out.println(rand1);
+		int rand2 = (int) (Math.random() * range) + min;
+		System.out.println(rand2);
+		int rand3 = (int) (Math.random() * range) + min;
+		System.out.println(rand3);
+		// find and print the minimum number
+		int minimum1 = Math.min (rand1, rand2);
+		int minimum = Math.min (rand3, minimum1);
+		System.out.println("The minimal generated number was " + minimum);
+    }
 }
