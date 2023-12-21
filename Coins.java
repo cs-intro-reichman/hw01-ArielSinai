2ef8981695395e1a7ffd1233349b3370696479a2
ArielSinai <ariel.sinai@post.runi.ac.il>
2023-12-21 14:06:08 +0200

    adding files

--- Coins.java
+++ Coins.java
@@ -1,9 +1,10 @@
-/*
- * Write a program that gets a quantity of cents as a command-line argument.
- * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
- */
-public class Coins {
-	public static void main(String[] args) {
-		// Put your code here
-	}
+public class Coins
+{
+    public static void main(String[] args) 
+	{
+        // decler integer + convert argument to integer
+		int a = Integer.parseInt(args[0]);
+		// calculate and print the quantity of quarters and cents
+		System.out.println("Use " + (a / 25) + " quarters and " + (a % 25) + " cents");
+    }
 }
--- Triangle.java
+++ Triangle.java
@@ -1,10 +1,19 @@
-/*
- * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
- * This is known as the Triangle Inequality Theorem. 
- * Write a program that tests if three given integers form a triangle.
- */ 
-public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public class Triangle
+{
+    public static void main(String[] args) 
+	{
+        // decler integers + convert argument to int
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		// check if triangle and prints true or false
+		if ((a + b > c) && (b + c > a) && (a + c > b)) 
+		{
+			System.out.println("true");
+		}
+		else 
+		{
+			System.out.println("false");
+		}
+    }
 }
